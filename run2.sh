5602493d64efa967a125875f1fedbc65b92a51cf
Dai Dongyang <daidongyang12@gmail.com>
2018-08-15 22:28:14 +0800

    update config

--- run2.sh
+++ run2.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-python main_crmodel.py --config_name=not_seq_len_weight --gpu=2 --vali_test_ses=2 --vali_type=F --test_type=M
-python main_crmodel.py --config_name=not_seq_len_weight --gpu=2 --vali_test_ses=2 --vali_type=M --test_type=F
+python main_crmodel.py --config_name=mix_data --gpu=2 --vali_test_ses=2 --vali_type=F --test_type=M
+python main_crmodel.py --config_name=mix_data --gpu=2 --vali_test_ses=2 --vali_type=M --test_type=F
+python main_crmodel.py --config_name=mix_data --gpu=2 --vali_test_ses=3 --vali_type=F --test_type=M
+python main_crmodel.py --config_name=mix_data --gpu=2 --vali_test_ses=3 --vali_type=M --test_type=F
