9880d041609613c5cdb0d36c9a9310aadab25680
Dai Dongyang <daidongyang12@gmail.com>
2018-08-26 10:06:12 +0800

    load data filter emos

--- run0.sh
+++ run0.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 
+python main_crmodel.py --config_name=not_seq_len_weight_shuffle_small_input --gpu=0 --vali_test_ses=0 --vali_type=F --test_type=M
+python main_crmodel.py --config_name=not_seq_len_weight_shuffle_small_input --gpu=0 --vali_test_ses=0 --vali_type=M --test_type=F
+python main_crmodel.py --config_name=not_seq_len_weight_shuffle_small_input --gpu=0 --vali_test_ses=4 --vali_type=F --test_type=M
--- run1.sh
+++ run1.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-python main_crmodel.py --config_name=not_seq_len_weight_shuffle_co_train --gpu=1 --vali_test_ses=1 --vali_type=F --test_type=M
-python main_crmodel.py --config_name=not_seq_len_weight_shuffle_co_train --gpu=1 --vali_test_ses=1 --vali_type=M --test_type=F
-python main_crmodel.py --config_name=not_seq_len_weight_shuffle_co_train --gpu=1 --vali_test_ses=4 --vali_type=M --test_type=F
+python main_crmodel.py --config_name=not_seq_len_weight_shuffle_small_input --gpu=1 --vali_test_ses=1 --vali_type=F --test_type=M
+python main_crmodel.py --config_name=not_seq_len_weight_shuffle_small_input --gpu=1 --vali_test_ses=1 --vali_type=M --test_type=F
+python main_crmodel.py --config_name=not_seq_len_weight_shuffle_small_input --gpu=1 --vali_test_ses=4 --vali_type=M --test_type=F
