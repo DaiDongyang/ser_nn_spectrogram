05031c3bc1086199780d3e12eb76f17f2abd1005
Dai Dongyang <daidongyang12@gmail.com>
2018-08-31 23:21:00 +0800

    learn tensorboard

--- tmp/learn_tensorboard.py
+++ tmp/learn_tensorboard.py
@@ -17,14 +17,13 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 FLAGS = None
-
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 def train():
     # Import data
     mnist = input_data.read_data_sets(FLAGS.data_dir, fake_data=FLAGS.fake_data)
-
     sess = tf.InteractiveSession()
-    # Create a multilayer model
+    # Create a multilayer model.
 
     # Input placeholders
     with tf.name_scope('input'):
@@ -33,12 +32,12 @@ def train():
 
     with tf.name_scope('input_reshape'):
         image_shaped_input = tf.reshape(x, [-1, 28, 28, 1])
-        tf.summary.image('input-image', image_shaped_input, 10)
+        tf.summary.image('input', image_shaped_input, 10)
 
     # We can't initialize these variables to 0 - the network will get stuck
     def weight_variable(shape):
-        """Create a weight varaible with appropriate initialization."""
-        initial = tf.truncated_normal(0.1, shape=shape)
+        """Create a weight variable with appropriate initialization."""
+        initial = tf.truncated_normal(shape, stddev=0.1)
         return tf.Variable(initial)
 
     def bias_variable(shape):
@@ -47,10 +46,9 @@ def train():
         return tf.Variable(initial)
 
     def variable_summaries(var):
-        """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+        """Attach a lot of summaries to a Tensor (for TensorBoard visualization)"""
         with tf.name_scope('summaries'):
             mean = tf.reduce_mean(var)
-            tf.summary.scalar('mean', mean)
             with tf.name_scope('stddev'):
                 stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
             tf.summary.scalar('stddev', stddev)
@@ -59,16 +57,14 @@ def train():
             tf.summary.histogram('histogram', var)
 
     def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
-        """
-        Reusable code for making a simple neural net layer.
-
-        It does a matrix multiply, bias add, and then uses ReLU to nonlinearize.
-        It also sets up name coping so that the resultant graph is easy to read,
+        """Reusable code for making a simple neural net layer.
+        It does a matrix multiply, bias add, and then use ReLU to nonlinearize.
+        It also sets up name scoping so that the resultant graph is easy to read,
         and adds a number of summary ops.
         """
-        # Adding a name scope ensures logical grouping of the layers n the graph.
+        # Adding a name scope ensures logical grouping of the layers in the graph.
         with tf.name_scope(layer_name):
-            # This Variable will hold the state of weights for the layer
+            # This Variable will hold the state of the weights for the layer
             with tf.name_scope('weights'):
                 weights = weight_variable([input_dim, output_dim])
                 variable_summaries(weights)
@@ -77,7 +73,7 @@ def train():
                 variable_summaries(biases)
             with tf.name_scope('Wx_plus_b'):
                 preactivate = tf.matmul(input_tensor, weights) + biases
-                tf.summary.histogram('pre_activations', preactivate)
+                tf.summary.histogram('pre_activateions', preactivate)
             activations = act(preactivate, name='activation')
             tf.summary.histogram('activations', activations)
             return activations
@@ -89,12 +85,13 @@ def train():
         tf.summary.scalar('dropout_keep_probability', keep_prob)
         dropped = tf.nn.dropout(hidden1, keep_prob)
 
-    # Do not apply softmax activation ye, see below.
+    # Do not apply softmax activation yet, see below.
     y = nn_layer(dropped, 500, 10, 'layer2', act=tf.identity)
 
     with tf.name_scope('cross_entropy'):
         with tf.name_scope('total'):
             cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=y)
+
     tf.summary.scalar('cross_entropy', cross_entropy)
 
     with tf.name_scope('train'):
@@ -105,17 +102,18 @@ def train():
             correct_prediction = tf.equal(tf.argmax(y, 1), y_)
         with tf.name_scope('accuracy'):
             accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    tf.summary.scalar('accuracy', accuracy)
+        tf.summary.scalar('accuracy', accuracy)
 
     # Merge all the summaries and write them out to
-    # /tmp/tensorflow/mnist/logs/mnist_weith_summaries(by default)
+    # /tmp/tensorflow/mnist/logs/mnist_with_summaries (by default)
     merged = tf.summary.merge_all()
-    train_writer = tf.summary.FileWriter(FLAGS.log_dir + 'train', sess.graph)
+    train_writer = tf.summary.FileWriter(FLAGS.log_dir + '/train', sess.graph)
+    test_writer = tf.summary.FileWriter(FLAGS.log_dir + '/test')
     tf.global_variables_initializer().run()
 
-    # Train the model, and alse write summaries.
-    # Every 10th step, measure test-set accuracy, and write test summaries.
-    # All other steps, run traijn_setep on training data, & add training summaries
+    # Train the model, and also write smmaries.
+    # Every 10th step, measure test-set accuracy, and write test summaries
+    # All other steps, run train_step on training data, & add training summaries
 
     def feed_dict(train):
         """Make a TensorFlow feed_dict: maps data onto Tensor placeholders."""
@@ -125,6 +123,62 @@ def train():
         else:
             xs, ys = mnist.test.images, mnist.test.labels
             k = 1.0
-
         return {x: xs, y_: ys, keep_prob: k}
 
+    for i in range(FLAGS.max_steps):
+        if i % 10 == 0:  # Record summaries and test-set accuracy
+            summary, acc = sess.run([merged, accuracy], feed_dict=feed_dict(False))
+            test_writer.add_summary(summary, i)
+            print('Accuracy at step %s: %s' % (i, acc))
+        else:  # Record train set summaries, and train
+            if i % 100 == 99:  # Record execute stats
+                # todo: 看看下面这两行是干嘛的
+                # run_option = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                # run_metadata = tf.RunMetadata()
+                summary, _ = sess.run([merged, train_step],
+                                      feed_dict=feed_dict(True),
+                                      )
+                # summary, _ = sess.run([merged, train_step],
+                #                       feed_dict=feed_dict(True),
+                #                       options=run_option,
+                #                       run_metadata=run_metadata)
+                # train_writer.add_run_metadata(run_metadata, 'step%3d' % i)
+                train_writer.add_summary(summary, i)
+                print('Adding run metadata for', i)
+            else:
+                summary, _ = sess.run([merged, train_step], feed_dict=feed_dict(True))
+                train_writer.add_summary(summary, i)
+    train_writer.close()
+    test_writer.close()
+
+
+def main(_):
+    if tf.gfile.Exists(FLAGS.log_dir):
+        tf.gfile.DeleteRecursively(FLAGS.log_dir)
+    tf.gfile.MakeDirs(FLAGS.log_dir)
+    train()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fake_data', nargs='?', const=True, type=bool, default=False,
+                        help='If true, use fake data for unit testing.')
+    parser.add_argument('--max_steps', type=int, default=10001,
+                        help='Number of steps to run trainer')
+    parser.add_argument('--learning_rate', type=float, default=0.001,
+                        help='Initial learning rate')
+    parser.add_argument('--dropout', type=float, default=0.9,
+                        help='Keep probability for training dropout')
+    parser.add_argument(
+        '--data_dir',
+        type=str,
+        default=os.path.join(os.getenv('TEST_TMPDIR', '/tmp'),
+                             'tensorflow/mnist/input_data'),
+        help='Directory for storing input data'
+    )
+    parser.add_argument('--log_dir', type=str,
+                        default=os.path.join(os.getenv('TEST_TMPDIR', '/tmp'),
+                                             'tensorflow/mnist/logs/mnist_with_summaries'),
+                        help='Summaries log directory')
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
