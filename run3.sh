05e5a054149fc5e13d04e09650c7c8cb8dcbd424
Dai Dongyang <daidongyang12@gmail.com>
2018-09-03 00:35:30 +0800

    update run

--- run3.sh
+++ run3.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-python main_crmodel.py --config_name=psnrnws_co_train --gpu=3 --vali_test_ses=3 --vali_type=F --test_type=M
-python main_crmodel.py --config_name=psnrnws_co_train --gpu=3 --vali_test_ses=3 --vali_type=M --test_type=F
-python main_crmodel.py --config_name=psnrnws_co_train --gpu=3 --vali_test_ses=4 --vali_type=F --test_type=M
+python main_crmodel.py --config_name=psnrnws --gpu=3 --vali_test_ses=3 --vali_type=F --test_type=M
+python main_crmodel.py --config_name=psnrnws --gpu=3 --vali_test_ses=3 --vali_type=M --test_type=F
+python main_crmodel.py --config_name=psnrnws --gpu=3 --vali_test_ses=4 --vali_type=F --test_type=M
 
 
