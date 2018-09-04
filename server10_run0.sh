9169f69178227afc1ae0778bb5a167362e1e7659
Dai Dongyang <daidongyang12@gmail.com>
2018-09-04 23:33:08 +0800

    update config

--- server10_run0.sh
+++ server10_run0.sh
@@ -4,8 +4,3 @@ python main_cr_v2.py --config_name=ce_m3_nw --gpu=0 --vali_test_ses=0 --vali_typ
 python main_cr_v2.py --config_name=ce_m3_nw --gpu=0 --vali_test_ses=1 --vali_type=F --test_type=M
 python main_cr_v2.py --config_name=ce_m3_nw --gpu=0 --vali_test_ses=1 --vali_type=M --test_type=F
 python main_cr_v2.py --config_name=ce_m3_nw --gpu=0 --vali_test_ses=2 --vali_type=F --test_type=M
-python main_cr_v2.py --config_name=ce_m3_nw --gpu=3 --vali_test_ses=2 --vali_type=M --test_type=F
-python main_cr_v2.py --config_name=ce_m3_nw --gpu=3 --vali_test_ses=3 --vali_type=F --test_type=M
-python main_cr_v2.py --config_name=ce_m3_nw --gpu=3 --vali_test_ses=3 --vali_type=M --test_type=F
-python main_cr_v2.py --config_name=ce_m3_nw --gpu=3 --vali_test_ses=4 --vali_type=F --test_type=M
-python main_cr_v2.py --config_name=ce_m3_nw --gpu=3 --vali_test_ses=4 --vali_type=M --test_type=F
---
+++ server10_run3.sh
@@ -0,0 +1,5 @@
+python main_cr_v2.py --config_name=ce_m3_nw --gpu=3 --vali_test_ses=2 --vali_type=M --test_type=F
+python main_cr_v2.py --config_name=ce_m3_nw --gpu=3 --vali_test_ses=3 --vali_type=F --test_type=M
+python main_cr_v2.py --config_name=ce_m3_nw --gpu=3 --vali_test_ses=3 --vali_type=M --test_type=F
+python main_cr_v2.py --config_name=ce_m3_nw --gpu=3 --vali_test_ses=4 --vali_type=F --test_type=M
+python main_cr_v2.py --config_name=ce_m3_nw --gpu=3 --vali_test_ses=4 --vali_type=M --test_type=F
