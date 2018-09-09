11800c12fd2ebb8ff2495451a8907ed208ea7b91
Dai Dongyang <daidongyang12@gmail.com>
2018-09-09 20:19:17 +0800

    update cfg

--- bashs/server11_run1.sh
+++ bashs/server11_run1.sh
@@ -5,13 +5,15 @@
 #python main_cr_v2.py --config_name=ce_m2_bn --gpu=1 --vali_test_ses=4 --vali_type=F --test_type=M
 #python main_cr_v2.py --config_name=ce_m2_bn --gpu=1 --vali_test_ses=4 --vali_type=M --test_type=F
 
-#python main_cr_v2.py --config_name=ce_center_m3_co_bn --gpu=1 --vali_test_ses=0 --vali_type=F --test_type=M
-#python main_cr_v2.py --config_name=ce_center_m3_co_bn --gpu=1 --vali_test_ses=0 --vali_type=M --test_type=F
-#python main_cr_v2.py --config_name=ce_center_m3_co_bn --gpu=1 --vali_test_ses=1 --vali_type=F --test_type=M
-#python main_cr_v2.py --config_name=ce_center_m3_co_bn --gpu=1 --vali_test_ses=1 --vali_type=M --test_type=F
-python main_cr_v2.py --config_name=ce_center_m3_co_bn --gpu=1 --vali_test_ses=2 --vali_type=F --test_type=M
-python main_cr_v2.py --config_name=ce_center_m3_co_bn --gpu=1 --vali_test_ses=2 --vali_type=M --test_type=F
-python main_cr_v2.py --config_name=ce_center_m3_co_bn --gpu=1 --vali_test_ses=3 --vali_type=F --test_type=M
-python main_cr_v2.py --config_name=ce_center_m3_co_bn --gpu=1 --vali_test_ses=3 --vali_type=M --test_type=F
-python main_cr_v2.py --config_name=ce_center_m3_co_bn --gpu=1 --vali_test_ses=4 --vali_type=F --test_type=M
-python main_cr_v2.py --config_name=ce_center_m3_co_bn --gpu=1 --vali_test_ses=4 --vali_type=M --test_type=F
+echo 'ce_center_m3_co'
+
+python main_cr_v2.py --config_name=ce_center_m3_co --gpu=1 --vali_test_ses=0 --vali_type=F --test_type=M
+python main_cr_v2.py --config_name=ce_center_m3_co --gpu=1 --vali_test_ses=0 --vali_type=M --test_type=F
+python main_cr_v2.py --config_name=ce_center_m3_co --gpu=1 --vali_test_ses=1 --vali_type=F --test_type=M
+python main_cr_v2.py --config_name=ce_center_m3_co --gpu=1 --vali_test_ses=1 --vali_type=M --test_type=F
+python main_cr_v2.py --config_name=ce_center_m3_co --gpu=1 --vali_test_ses=2 --vali_type=F --test_type=M
+python main_cr_v2.py --config_name=ce_center_m3_co --gpu=1 --vali_test_ses=2 --vali_type=M --test_type=F
+python main_cr_v2.py --config_name=ce_center_m3_co --gpu=1 --vali_test_ses=3 --vali_type=F --test_type=M
+python main_cr_v2.py --config_name=ce_center_m3_co --gpu=1 --vali_test_ses=3 --vali_type=M --test_type=F
+python main_cr_v2.py --config_name=ce_center_m3_co --gpu=1 --vali_test_ses=4 --vali_type=F --test_type=M
+python main_cr_v2.py --config_name=ce_center_m3_co --gpu=1 --vali_test_ses=4 --vali_type=M --test_type=F
