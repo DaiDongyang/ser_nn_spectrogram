1f7c72224746c897ce073af24b79705807d899a2
Dai Dongyang <daidongyang12@gmail.com>
2018-09-18 15:34:14 +0800

    update cfg

--- bashs/server10_run2.sh
+++ bashs/server10_run2.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-#python main_cr_v2.py  --config_name=ce_m1 --gpu=2 --vali_test_ses=3 --vali_type=F --test_type=M
-#python main_cr_v2.py  --config_name=ce_m1 --gpu=2 --vali_test_ses=3 --vali_type=M --test_type=F
-#python main_cr_v2.py  --config_name=ce_m1 --gpu=2 --vali_test_ses=4 --vali_type=F --test_type=M
-#python main_cr_v2.py  --config_name=ce_m1 --gpu=2 --vali_test_ses=4 --vali_type=M --test_type=F
+echo 'ce_center_m5_lambda030alpha050beta015gamma010'
 
-python main_cr_v2.py --config_name=ce_ma --gpu=2 --vali_test_ses=0 --vali_type=F --test_type=M
-python main_cr_v2.py --config_name=ce_ma --gpu=2 --vali_test_ses=0 --vali_type=M --test_type=F
-python main_cr_v2.py --config_name=ce_ma --gpu=2 --vali_test_ses=1 --vali_type=F --test_type=M
-python main_cr_v2.py --config_name=ce_ma --gpu=2 --vali_test_ses=1 --vali_type=M --test_type=F
-python main_cr_v2.py --config_name=ce_ma --gpu=2 --vali_test_ses=2 --vali_type=F --test_type=M
-python main_cr_v2.py --config_name=ce_ma --gpu=2 --vali_test_ses=2 --vali_type=M --test_type=F
-python main_cr_v2.py --config_name=ce_ma --gpu=2 --vali_test_ses=3 --vali_type=F --test_type=M
-python main_cr_v2.py --config_name=ce_ma --gpu=2 --vali_test_ses=3 --vali_type=M --test_type=F
-python main_cr_v2.py --config_name=ce_ma --gpu=2 --vali_test_ses=4 --vali_type=F --test_type=M
-python main_cr_v2.py --config_name=ce_ma --gpu=2 --vali_test_ses=4 --vali_type=M --test_type=F
+python main_cr_v2.py --config_file=./cr_model_v2/impro.yml --config_name=ce_center_m5_lambda030alpha050beta015gamma010 --gpu=2 --vali_test_ses=0 --vali_type=F --test_type=M
+python main_cr_v2.py --config_file=./cr_model_v2/impro.yml --config_name=ce_center_m5_lambda030alpha050beta015gamma010 --gpu=2 --vali_test_ses=0 --vali_type=M --test_type=F
+python main_cr_v2.py --config_file=./cr_model_v2/impro.yml --config_name=ce_center_m5_lambda030alpha050beta015gamma010 --gpu=2 --vali_test_ses=1 --vali_type=F --test_type=M
+python main_cr_v2.py --config_file=./cr_model_v2/impro.yml --config_name=ce_center_m5_lambda030alpha050beta015gamma010 --gpu=2 --vali_test_ses=1 --vali_type=M --test_type=F
+python main_cr_v2.py --config_file=./cr_model_v2/impro.yml --config_name=ce_center_m5_lambda030alpha050beta015gamma010 --gpu=2 --vali_test_ses=2 --vali_type=F --test_type=M
+#python main_cr_v2.py --config_file=./cr_model_v2/impro.yml --config_name=ce_center_m5_lambda030alpha050beta015gamma010 --gpu=2 --vali_test_ses=2 --vali_type=M --test_type=F
+#python main_cr_v2.py --config_file=./cr_model_v2/impro.yml --config_name=ce_center_m5_lambda030alpha050beta015gamma010 --gpu=2 --vali_test_ses=3 --vali_type=F --test_type=M
+#python main_cr_v2.py --config_file=./cr_model_v2/impro.yml --config_name=ce_center_m5_lambda030alpha050beta015gamma010 --gpu=2 --vali_test_ses=3 --vali_type=M --test_type=F
+#python main_cr_v2.py --config_file=./cr_model_v2/impro.yml --config_name=ce_center_m5_lambda030alpha050beta015gamma010 --gpu=2 --vali_test_ses=4 --vali_type=F --test_type=M
+#python main_cr_v2.py --config_file=./cr_model_v2/impro.yml --config_name=ce_center_m5_lambda030alpha050beta015gamma010 --gpu=2 --vali_test_ses=4 --vali_type=M --test_type=F
