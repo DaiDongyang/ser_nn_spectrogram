7e99971d362b1d808386542a07abe7e77f59f59c
Dai Dongyang <daidongyang12@gmail.com>
2018-09-19 16:37:55 +0800

    debug center loss2

--- cr_model_v2/cr_model_run.py
+++ cr_model_v2/cr_model_run.py
@@ -72,12 +72,20 @@ class CRModelRun(object):
                                                    name='ce_loss_ph')
         self.loss_ph_d['center_loss'] = tf.placeholder(self.tf_float_type, shape=[],
                                                        name='center_loss_ph')
+        self.loss_ph_d['center_loss2'] = tf.placeholder(self.tf_float_type, shape=[],
+                                                        name='center_loss2_ph')
+        self.loss_ph_d['center_loss3'] = tf.placeholder(self.tf_float_type, shape=[],
+                                                        name='center_loss3_ph')
         self.loss_ph_d['cos_loss'] = tf.placeholder(self.tf_float_type, shape=[],
                                                     name='cos_loss_ph')
         self.loss_ph_d['dist_loss'] = tf.placeholder(self.tf_float_type, shape=[],
                                                      name='dist_loss_ph')
         self.loss_ph_d['ce_center_loss'] = tf.placeholder(self.tf_float_type, shape=[],
                                                           name='ce_center_loss_ph')
+        self.loss_ph_d['ce_center_loss2'] = tf.placeholder(self.tf_float_type, shape=[],
+                                                           name='ce_center_loss2_ph')
+        self.loss_ph_d['ce_center_loss3'] = tf.placeholder(self.tf_float_type, shape=[],
+                                                           name='ce_center_loss3_ph')
         self.loss_ph_d['ce_cos_loss'] = tf.placeholder(self.tf_float_type, shape=[],
                                                        name='ce_cos_loss_ph')
         self.loss_ph_d['ce_dist_loss'] = tf.placeholder(self.tf_float_type, shape=[],
@@ -374,6 +382,25 @@ class CRModelRun(object):
                             model.center_loss_gamma_ph: var_hps.center_loss_gamma,
                             model.feature_norm_alpha_ph: var_hps.feature_norm_alpha,
                         })
+                # todo: debug
+                debug_dict = session.run(model.debug_dict,
+                                         feed_dict={
+                                             model.fc_kprob_ph: self.hps.fc_kprob,
+                                             model.lr_ph: var_hps.lr,
+                                             model.x_ph: batch_input.x.astype(self.np_float_type),
+                                             model.t_ph: batch_input.t,
+                                             model.e_ph: batch_input.e,
+                                             model.e_w_ph: batch_input.w.astype(self.np_float_type),
+                                             model.is_training_ph: True,
+                                             model.cos_loss_lambda_ph: var_hps.cos_loss_lambda,
+                                             model.dist_loss_lambda_ph: var_hps.dist_loss_lambda,
+                                             model.center_loss_lambda_ph: var_hps.center_loss_lambda,
+                                             model.center_loss_alpha_ph: var_hps.center_loss_alpha,
+                                             model.center_loss_beta_ph: var_hps.center_loss_beta,
+                                             model.center_loss_gamma_ph: var_hps.center_loss_gamma,
+                                             model.feature_norm_alpha_ph: var_hps.feature_norm_alpha,
+                                         })
+                print('%% debug dict:', debug_dict)
                 self.logger.log('step %d,' % i, 'input shape', batch_input.x.shape, 'e_acc',
                                 batch_e_acc, 'batch_loss_d', batch_loss_d, level=2)
             else:
