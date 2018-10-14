0e2fa17af977e4e13866f22a018b608ad2bc053a
Dai Dongyang <daidongyang12@gmail.com>
2018-10-14 13:32:42 +0800

    update

--- bashs/server11_run2.sh
+++ bashs/server11_run2.sh
@@ -1,51 +1,36 @@
 #!/usr/bin/env bash
 
-echo 'run2'
+echo 'test alpha'
 
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda003_nodropout --gpu=2 --vali_type='0' --test_type='1'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda003_nodropout --gpu=2 --vali_type='2' --test_type='3'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda003_nodropout --gpu=2 --vali_type='4' --test_type='5'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda003_nodropout --gpu=2 --vali_type='6' --test_type='7'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda003_nodropout --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha01 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha02 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha03 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha04 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha05 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha06 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha07 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha08 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha10 --gpu=2 --vali_type='8' --test_type='9'
 
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda001_nodropout --gpu=2 --vali_type='0' --test_type='1'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda001_nodropout --gpu=2 --vali_type='2' --test_type='3'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda001_nodropout --gpu=2 --vali_type='4' --test_type='5'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda001_nodropout --gpu=2 --vali_type='6' --test_type='7'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda001_nodropout --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha01 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha02 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha03 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha04 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha05 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha06 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha07 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha08 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha10 --gpu=2 --vali_type='8' --test_type='9'
 
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda0003_nodropout --gpu=2 --vali_type='0' --test_type='1'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda0003_nodropout --gpu=2 --vali_type='2' --test_type='3'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda0003_nodropout --gpu=2 --vali_type='4' --test_type='5'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda0003_nodropout --gpu=2 --vali_type='6' --test_type='7'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda0003_nodropout --gpu=2 --vali_type='8' --test_type='9'
-
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda0001_nodropout --gpu=2 --vali_type='0' --test_type='1'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda0001_nodropout --gpu=2 --vali_type='2' --test_type='3'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda0001_nodropout --gpu=2 --vali_type='4' --test_type='5'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda0001_nodropout --gpu=2 --vali_type='6' --test_type='7'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda0001_nodropout --gpu=2 --vali_type='8' --test_type='9'
-
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda01_nodropout --gpu=2 --vali_type='0' --test_type='1'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda01_nodropout --gpu=2 --vali_type='2' --test_type='3'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda01_nodropout --gpu=2 --vali_type='4' --test_type='5'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda01_nodropout --gpu=2 --vali_type='6' --test_type='7'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda01_nodropout --gpu=2 --vali_type='8' --test_type='9'
-
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda03_nodropout --gpu=2 --vali_type='0' --test_type='1'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda03_nodropout --gpu=2 --vali_type='2' --test_type='3'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda03_nodropout --gpu=2 --vali_type='4' --test_type='5'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda03_nodropout --gpu=2 --vali_type='6' --test_type='7'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda03_nodropout --gpu=2 --vali_type='8' --test_type='9'
-
-
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda1_nodropout --gpu=2 --vali_type='0' --test_type='1'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda1_nodropout --gpu=2 --vali_type='2' --test_type='3'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda1_nodropout --gpu=2 --vali_type='4' --test_type='5'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda1_nodropout --gpu=2 --vali_type='6' --test_type='7'
-python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64.yml --config_name=ce_center_m11_origin_lambda1_nodropout --gpu=2 --vali_type='8' --test_type='9'
-
-
-
-
-
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha01 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha02 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha03 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha04 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha05 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha06 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha07 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha08 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=2 --vali_type='8' --test_type='9'
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_impro_batch64_nodropout.yml --config_name=ce_center_m11_origin_lambda03_alpha10 --gpu=2 --vali_type='8' --test_type='9'
