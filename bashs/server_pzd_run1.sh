70e78b2d04d26a8cc8434d950233bd8117191a9b
Dai Dongyang <daidongyang12@gmail.com>
2018-10-27 16:33:34 +0800

    update config for stft lambda03alpha05

--- bashs/server_pzd_run1.sh
+++ bashs/server_pzd_run1.sh
@@ -1,61 +1,89 @@
 #!/usr/bin/env bash
-echo 'method1'
 
-echo 'method1' > run1-server-pzd.log
-echo 'beta 0.04 gamma 0.2'
 
-echo 'beta 0.04 gamma 0.2' >> run1-server-pzd.log
+echo 'ce_center_m11_origin_lambda03_alpha09' > run1-server-pzd.log
 
-python main_cr_mel_method1.py --gpu=1 --vali_type='8' --test_type='9' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+echo 'cross validation 1: ...' >> run1-server-pzd.log
+
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='8' --test_type='9'
 echo 'v8t9' >> run1-server-pzd.log
 
-python main_cr_mel_method1.py --gpu=1 --vali_type='6' --test_type='7' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='6' --test_type='7'
 echo 'v6t7' >> run1-server-pzd.log
 
-python main_cr_mel_method1.py --gpu=1 --vali_type='4' --test_type='5' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='4' --test_type='5'
 echo 'v4t5' >> run1-server-pzd.log
 
-python main_cr_mel_method1.py --gpu=1 --vali_type='2' --test_type='3' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='2' --test_type='3'
 echo 'v2t3' >> run1-server-pzd.log
 
-python main_cr_mel_method1.py --gpu=1 --vali_type='0' --test_type='1' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='0' --test_type='1'
 echo 'v0t1' >> run1-server-pzd.log
 
+echo 'cross validation 2: ...' >> run1-server-pzd.log
 
-echo 'beta 0.04 gamma 0.2' >> run1-server-pzd.log
-
-python main_cr_mel_method1.py --gpu=1 --vali_type='8' --test_type='9' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='8' --test_type='9'
 echo 'v8t9' >> run1-server-pzd.log
 
-python main_cr_mel_method1.py --gpu=1 --vali_type='6' --test_type='7' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='6' --test_type='7'
 echo 'v6t7' >> run1-server-pzd.log
 
-python main_cr_mel_method1.py --gpu=1 --vali_type='4' --test_type='5' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='4' --test_type='5'
 echo 'v4t5' >> run1-server-pzd.log
 
-python main_cr_mel_method1.py --gpu=1 --vali_type='2' --test_type='3' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='2' --test_type='3'
 echo 'v2t3' >> run1-server-pzd.log
 
-python main_cr_mel_method1.py --gpu=1 --vali_type='0' --test_type='1' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='0' --test_type='1'
 echo 'v0t1' >> run1-server-pzd.log
 
+echo 'cross validation 3: ...' >> run1-server-pzd.log
 
-echo 'method1' > run1-server-pzd.log
-
-echo 'beta 0.04 gamma 0.2' >> run1-server-pzd.log
-
-python main_cr_mel_method1.py --gpu=1 --vali_type='8' --test_type='9' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='8' --test_type='9'
 echo 'v8t9' >> run1-server-pzd.log
 
-python main_cr_mel_method1.py --gpu=1 --vali_type='6' --test_type='7' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='6' --test_type='7'
 echo 'v6t7' >> run1-server-pzd.log
 
-python main_cr_mel_method1.py --gpu=1 --vali_type='4' --test_type='5' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='4' --test_type='5'
 echo 'v4t5' >> run1-server-pzd.log
 
-python main_cr_mel_method1.py --gpu=1 --vali_type='2' --test_type='3' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='2' --test_type='3'
 echo 'v2t3' >> run1-server-pzd.log
 
-python main_cr_mel_method1.py --gpu=1 --vali_type='0' --test_type='1' --beta=0.04 --gamma=0.2 --config_name=server_pzd
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='0' --test_type='1'
 echo 'v0t1' >> run1-server-pzd.log
 
+echo 'cross validation 4: ...' >> run1-server-pzd.log
+
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='8' --test_type='9'
+echo 'v8t9' >> run1-server-pzd.log
+
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='6' --test_type='7'
+echo 'v6t7' >> run1-server-pzd.log
+
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='4' --test_type='5'
+echo 'v4t5' >> run1-server-pzd.log
+
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='2' --test_type='3'
+echo 'v2t3' >> run1-server-pzd.log
+
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='0' --test_type='1'
+echo 'v0t1' >> run1-server-pzd.log
+
+echo 'cross validation 5: ...' >> run1-server-pzd.log
+
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='8' --test_type='9'
+echo 'v8t9' >> run1-server-pzd.log
+
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='6' --test_type='7'
+echo 'v6t7' >> run1-server-pzd.log
+
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='4' --test_type='5'
+echo 'v4t5' >> run1-server-pzd.log
+
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='2' --test_type='3'
+echo 'v2t3' >> run1-server-pzd.log
+
+python main_cr_mel.py --config_file=./cr_model_v2/cfgs/mel_rediv_ma_batch32_nodropout_pzd.yml --config_name=ce_center_m11_origin_lambda03_alpha09 --gpu=1 --vali_type='0' --test_type='1'
+echo 'v0t1' >> run1-server-pzd.log
